2179c0114988671a307cc9b66889a8545baa4c51
David Cosby <60022092@ironschools.org>
2018-09-19 10:19:53 -0600

    added Ex_1_9.java

---
+++ Chapter 1/Ex_1_9.java
@@ -0,0 +1,13 @@
+/*
+Author: David Cosby
+Date: 9.19.18
+
+Outputs the area and perimeter of a rectangle
+*/
+
+public class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println((4.5 + 7.9) * 2);
+	}
+}
