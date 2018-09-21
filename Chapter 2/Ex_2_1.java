9626787380645bd29fbddb3319c13a606474367e
David Cosby <60022092@ironschools.org>
2018-09-21 08:15:02 -0600

    added Ex_2_1.java
    not yet finished.

---
+++ Chapter 2/Ex_2_1.java
@@ -0,0 +1,8 @@
+class Untitled {
+	public static void main(String[] args) {
+		System.out.println("Enter a degree in Celsius: ");
+		double celsius = 29.5; // placeholder until I figure out user input
+		double fahrenheit = (9.0/5.0) * celsius + 32.0;
+		System.out.println(fahrenheit);
+	}
+}
