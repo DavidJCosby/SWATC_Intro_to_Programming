087cb3eca8a39db0e5e8f2f0bed32bb119ff2f8f
David Cosby <60022092@ironschools.org>
2018-09-21 08:30:13 -0600

    added name and date at top

--- Chapter 2/Ex_2_1.java
+++ Chapter 2/Ex_2_1.java
@@ -1,3 +1,12 @@
+/*
+Author: David Cosby
+Date: 9.21.18
+
+Takes a celsius measurement from the user and converts it into fahrenheit
+
+*/
+
+
 class Untitled {
 	public static void main(String[] args) {
 		System.out.println("Enter a degree in Celsius: ");
