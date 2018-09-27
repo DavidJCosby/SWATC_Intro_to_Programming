1ed00b7baa844c21f23eb3b51c91f2ea2544814a
David Cosby <60022092@ironschools.org>
2018-09-27 08:24:40 -0600

    finished Ex_2_13.java

--- Chapter 2/Ex_2_13.java
+++ Chapter 2/Ex_2_13.java
@@ -1,14 +1,32 @@
 /*
 Author: David Cosby
-Date: 9.21.18
+Date: 9.27.18
 
-Takes a celsius measurement from the user and converts it into fahrenheit
+Calculates account balance over a period of six months, given monthly
+deposits and annual interest rates.
 
 */
+
+import java.util.Scanner;
 
 
 class Ex_2_13 {
 	public static void main(String[] args) {
+		final double ANNUAL_INTEREST_RATE = 0.05;
+		double monthlyInterestRate = ANNUAL_INTEREST_RATE / 12;
+		double accountBalance = 0;
 
+		Scanner input = new Scanner(System.in);
+		System.out.print("How much do you deposit each month? $");
+		double monthlyDeposit = input.nextDouble();
+
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate); // step
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		
+		System.out.println("final balance after 6 months: $" + accountBalance);
 	}
 }
