7356bd07872d4e94d05395afd5b003c3ebd2b18e
David Cosby <60022092@ironschools.org>
2018-09-27 07:56:32 -0600

    added user input into the temperature converter

--- Chapter 2/Ex_2_1.java
+++ Chapter 2/Ex_2_1.java
@@ -5,13 +5,14 @@ Date: 9.21.18
 Takes a celsius measurement from the user and converts it into fahrenheit
 
 */
+import java.util.Scanner;
 
-
-class Ex_2_1 {
+public class Ex_2_1 {
 	public static void main(String[] args) {
-		System.out.println("Enter a degree in Celsius: ");
-		double celsius = 29.5; // placeholder until I figure out user input
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a degree in Celsius: ");
+		double celsius = input.nextDouble();
 		double fahrenheit = (9.0/5.0) * celsius + 32.0;
-		System.out.println(fahrenheit);
+		System.out.println(celsius + " degrees celsius is equal to " + fahrenheit + " degrees fahrenheit.");
 	}
 }
---
+++ Chapter 2/Ex_2_13.java
@@ -0,0 +1,14 @@
+/*
+Author: David Cosby
+Date: 9.21.18
+
+Takes a celsius measurement from the user and converts it into fahrenheit
+
+*/
+
+
+class Ex_2_13 {
+	public static void main(String[] args) {
+
+	}
+}
