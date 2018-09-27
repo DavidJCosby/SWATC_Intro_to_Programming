b3f5db58c1d400ac121f0114b009ec8312269974
David Cosby <60022092@ironschools.org>
2018-09-27 08:32:49 -0600

    Changed some variables to constants in Ex_2_13.java

--- Chapter 2/Ex_2_13.java
+++ Chapter 2/Ex_2_13.java
@@ -13,19 +13,19 @@ import java.util.Scanner;
 class Ex_2_13 {
 	public static void main(String[] args) {
 		final double ANNUAL_INTEREST_RATE = 0.05;
-		double monthlyInterestRate = ANNUAL_INTEREST_RATE / 12;
+		final double MONTHLY_INTEREST_RATE = ANNUAL_INTEREST_RATE / 12;
 		double accountBalance = 0;
-
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("How much do you deposit each month? $");
-		double monthlyDeposit = input.nextDouble();
+		final double MONTHLY_DEPOSIT = input.nextDouble();
 
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate); // step
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
-		accountBalance = (accountBalance + monthlyDeposit) * (1 + monthlyInterestRate);
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE); // step
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE);
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE);
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE);
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE);
+		accountBalance = (accountBalance + MONTHLY_DEPOSIT) * (1 + MONTHLY_INTEREST_RATE);
 		
 		System.out.println("final balance after 6 months: $" + accountBalance);
 	}
