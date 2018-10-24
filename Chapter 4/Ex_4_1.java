9e76917bce212e472118b9c22e14a5d51407156b
David Cosby <60022092@ironschools.org>
2018-10-24 10:36:59 -0600

    added Ex_4_1

---
+++ Chapter 4/Ex_4_1.java
@@ -0,0 +1,26 @@
+/*
+Author: David Cosby
+Date: 10.24.18
+Description:
+	Calculates the area of a pentagon, given r
+*/
+
+import java.util.Scanner;
+
+class Ex_4_1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the length from the center to a vertex: ");
+		double radius = input.nextDouble();
+		
+		final double PI = Math.PI;
+		
+		final double SIDE_LENGTH = (2.0 * radius) * Math.sin(PI/5.0);
+		final double AREA =	
+			(5.0 * Math.pow(SIDE_LENGTH, 2.0)) /
+			(4.0 * Math.tan(PI/5.0));
+		
+		System.out.println("The area of the pentagon is " + AREA);
+	}
+}
