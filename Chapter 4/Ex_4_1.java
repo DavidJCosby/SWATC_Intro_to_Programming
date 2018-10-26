303e29ca4d7d408ec374f7178761d792e36024e9
David Cosby <60022092@ironschools.org>
2018-10-26 07:56:40 -0600

    removed PI declaration.

--- Chapter 4/Ex_4_1.java
+++ Chapter 4/Ex_4_1.java
@@ -2,7 +2,7 @@
 Author: David Cosby
 Date: 10.24.18
 Description:
-	Calculates the area of a pentagon, given r
+	Calculates the area of a pentagon, given r.
 */
 
 import java.util.Scanner;
@@ -14,12 +14,10 @@ class Ex_4_1 {
 		System.out.print("Enter the length from the center to a vertex: ");
 		double radius = input.nextDouble();
 		
-		final double PI = Math.PI;
-		
-		final double SIDE_LENGTH = (2.0 * radius) * Math.sin(PI/5.0);
+		final double SIDE_LENGTH = (2.0 * radius) * Math.sin(Math.PI/5.0);
 		final double AREA =	
 			(5.0 * Math.pow(SIDE_LENGTH, 2.0)) /
-			(4.0 * Math.tan(PI/5.0));
+			(4.0 * Math.tan(Math.PI/5.0));
 		
 		System.out.println("The area of the pentagon is " + AREA);
 	}
