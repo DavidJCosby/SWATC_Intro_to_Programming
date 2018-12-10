77c0c27043b7bf649212624dd21c918e3807a396
David Cosby <60022092@ironschools.org>
2018-12-10 08:56:16 -0700

    more work done

--- Random Stuff/AlgebraSolving.java
+++ Random Stuff/AlgebraSolving.java
@@ -1,37 +1,74 @@
+class Group {
+	Symbol[] contents;
+	
+	public Group(Symbol[] formula) {
+		contents = formula;
+	}
+	
+	//public Group simplify() {
+		
+	//}
+
+}
+
 class Symbol {
 	String character;
 	double value;
-	String type;
+	double coefficient = 1;
+	boolean hasValue = false;
+	
+	public Symbol(String s) {
+		character = s;
+	}
+	
+	public String toString() {
+		String returnValue = "";
+		if (coefficient == 0 || character == null) {
+			return returnValue;
+		}
 		
-	public void Variable(String s, double v) {
-		character = s;
-		value = v;
-		type = "Variable";
+		if (coefficient != 1) {
+			returnValue += coefficient;
+		}
+		returnValue += character;
+		
+		return returnValue;
 	}
 	
-	public void Constant(double v) {
-		value = v;
-		type = "Constant";
-	}
-	
-	public void Operator(String s) {
-		character = s;
+	public Symbol tryConvertToConstant() {
+		if (!hasValue) {
+			return this;
+		}
+		Double newValue = (coefficient * value);
+		Symbol converted = new Symbol("" + newValue);
+		converted.value = newValue;
+		return converted;		
 	}
 }
 
 
 
 class Equation {
+	public static Group formulaToGroup(String formula) {
+		String[] separatedFormula = formula.split("\\s+");
+		Symbol[] convertedSymbols};
+		
+		int i = 0;
+		for (String symbol : separatedFormula) {
+			i++;
+			convertedSymbols[i] = new Symbol(symbol);
+		}
+		return new Group(convertedSymbols);
+	}
 	
-	Symbol[] equation;
-
+	Group leftSide;
+	Group rightSide;
 	
 	public Equation(String formula) {
-		String[] separatedFormula = formula.split("\\s+");
-		
+		Group separatedFormula = formulaToGroup(formula);
 	}
 	
 	public static void main(String[] args) {
-		
+		Equation testEQ = new Equation("y = mx + b");
 	}
 }
