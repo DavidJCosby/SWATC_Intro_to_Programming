1f3bcec219ba01692ef9a573c735dfb4db3bfe2f
David Cosby <60022092@ironschools.org>
2019-03-08 08:58:50 -0700

    Adjusted Ex_8_37.java to better meet assignment requirements.

--- Chapter 8/Ex_8_37.java
+++ Chapter 8/Ex_8_37.java
@@ -8,7 +8,7 @@ Quizzes the user on various state capitals.
 import java.util.Scanner;
 
 class Ex_8_37 {
-	
+		
 	public static String[][] stateCapitalPairs = new String[][] {
 		{"Alabama", "Montgomery"},
 		{"California", "Sacramento"},
@@ -22,22 +22,11 @@ class Ex_8_37 {
 		{"South Dakota", "Pierre"},
 	};
 	
-	public static int randomInt(int min, int max) {
-		int range = max - min;
-		int num = min + (int) Math.round((Math.random() * range));
-		return num;
-	}
-	
-	public static String[] getRandomStateCapitalPair() {
-		int index = randomInt(0, 9);
-		return stateCapitalPairs[index];
-	}
-	
 	public static boolean checkEquivalent(String answer, String capital) {
 		capital = capital.toLowerCase();
 		answer = answer.toLowerCase();
 		
-		return(answer.equals(capital));
+		return (answer.equals(capital));
 	}
 	
 	public static String askUserForCapitalOf(String state) {
@@ -48,8 +37,7 @@ class Ex_8_37 {
 		return input.next();
 	}
 	
-	public static void QuizUser() {
-		String[] stateCapitalPair = getRandomStateCapitalPair();
+	public static void quizUserOnState(String[] stateCapitalPair) {
 		String state = stateCapitalPair[0];
 		String capital = stateCapitalPair[1];
 		
@@ -57,36 +45,25 @@ class Ex_8_37 {
 		boolean correct = checkEquivalent(response, capital);
 		
 		if (correct) {
-			System.out.println("Correct!");
-		}
-		else {
-			System.out.println("Incorrect.");
+			score ++;
 		}
 	}
 	
-	public static boolean checkIfBored() {
-		Scanner input = new Scanner(System.in);
-	
-		System.out.print("\n Another? (Y/N) ");
-		String response = input.next().toLowerCase();
-		
-		if (response.equals("y")) {
-			return false;
-		}
-		else {
-			return true;
+	public static void quizUserOnStates() {
+		for (String[] stateCapitalPair : stateCapitalPairs) {
+			quizUserOnState(stateCapitalPair);
 		}
 	}
+	
+	public static void showScore() {
+		System.out.println("\nYou scored " + score + "/10.");
+	}
+	
+	
+	public static int score = 0;
 		
 	public static void main(String[] args) {
-		boolean bored = false;
-		while (!bored) {
-			QuizUser();
-			bored = checkIfBored();
-			
-			if (!bored) {
-				System.out.print("\n");
-			}
-		}
+		quizUserOnStates();
+		showScore();
 	}
 }
